c8b6a1638b85c1857a128f5e872eb9bd240cde15
Towhidul Islam <m.towhid.islam@gmail.com>
2018-09-20 16:15:52 +0600

    ...

--- src/main/java/com/example/lab/controller/ChatController.java
+++ src/main/java/com/example/lab/controller/ChatController.java
@@ -1,12 +1,15 @@
 package com.example.lab.controller;
 
 import com.example.lab.model.ChatMessage;
+import com.example.lab.model.Condition;
 import com.example.lab.model.Info;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.stereotype.Controller;
 
 /**
@@ -32,11 +35,37 @@ public class ChatController {
 
     @MessageMapping("/chat.info/{info}")
     @SendTo("/topic/info")
-    public Info systemInfo(@DestinationVariable("info") String info){
+    public Info systemInfo(@DestinationVariable("info") String info, @Payload Info infoMessage){
         //System.out.println("info:"+info);
         Info inf = new Info();
-        inf.setInformation(info);
+        inf.setInformation(info+"##"+infoMessage.getInformation());
         return inf;
+    }
+
+    @Autowired
+    private SimpMessageSendingOperations messagingTemplate;
+
+    @MessageMapping("/chat.condition")
+    public void conditionalMessage(@Payload Condition condition){
+        System.out.println(condition.getValue()+":"+condition.getMessage());
+        if (condition.getValue().equalsIgnoreCase(ChatMessage.MessageType.CHAT.name())){
+            ChatMessage message = new ChatMessage();
+            message.setSender("Unknown");
+            message.setType(ChatMessage.MessageType.CHAT);
+            message.setContent(condition.getMessage());
+            messagingTemplate.convertAndSend("/topic/public", message);
+        }
+        else if(condition.getValue().equalsIgnoreCase(ChatMessage.MessageType.JOIN.name())){
+            Info inf = new Info();
+            inf.setInformation("JOIN"+"##"+condition.getMessage());
+            messagingTemplate.convertAndSend("/topic/info", inf);
+        }
+        else{
+            Info inf = new Info();
+            inf.setInformation("ITSOUL"+"##"+condition.getMessage());
+            messagingTemplate.convertAndSend("/topic/soul", inf);
+        }
+
     }
 
 }
---
+++ src/main/java/com/example/lab/model/Condition.java
@@ -0,0 +1,22 @@
+package com.example.lab.model;
+
+public class Condition {
+    private String message;
+    private String value;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
